2ac67cd410e911f8c4fbfe65da1cc6cc3f5bbf17
David Garavito <DGaravito@users.noreply.github.com>
2024-09-08 11:23:40 -0700

    Added data and basic functions

--- combine.py
+++ combine.py
@@ -1,7 +1,42 @@
-import pandas
+import pandas as pd
+from pathlib import Path
+import os
+
+
+def cleaner(dataset):
+
+    cleaned = dataset
+
+    return cleaned
+
+
+def processor(data):
+
+    joined_data = pd.DataFrame()
+
+    for state in data:
+
+        path = str(Path('Data/' + state).resolve())
+
+        df = pd.read_excel(path)
+
+        cleaned_data = cleaner(df)
+
+        joined_data = pd.concat(
+            [
+                joined_data, cleaned_data
+            ]
+        )
+
+    return joined_data
 
 
 def main():
 
-    print('fuck')
+    datapath = str(Path('Data').resolve())
 
+    datalist = os.listdir(datapath)
+
+    finaldata = processor(datalist)
+
+    finaldata.to_csv('animal.csv')
